29f723b08d399864039a47811fbb9010d17ae0e9
davidpr1822 <davidpinorojas18@gmail.com>
2026-04-17 14:31:35 +0200

    consultas hotel david P

---
+++ consultas_hotel.sql
@@ -0,0 +1,15 @@
+-- ─────────────────────────────────────────────
+-- CONSULTA 1
+-- Ver todos los clientes
+-- ─────────────────────────────────────────────
+SELECT nombre, telefono, email
+FROM cliente;
+
+
+-- ─────────────────────────────────────────────
+-- CONSULTA 2
+-- Ver todas las habitaciones
+-- ─────────────────────────────────────────────
+SELECT numero_habitacion, tipo, precio_noche
+FROM habitacion;
+
